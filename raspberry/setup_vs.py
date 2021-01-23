ba0e046f1ef3e298dfdd86687db107c1919ab339
Alessandro <a.capurso23@studenti.uniba.it>
2021-01-23 16:01:14 +0100

    fixed setup for env raspberry created setup for vital raspberry

--- raspberry/setup_vs.py
+++ raspberry/setup_vs.py
@@ -1,11 +1,12 @@
 from typing import Tuple
-import os.path
 import requests
 import json
+import os
 
-SETTINGS_FILE_NAME = "settings.cfg"
+SETTINGS_FILE_NAME = "settings_vs.cfg"
 PROTOCOL = "http://"
-ENPOINT_ROOM = "/room"
+ENDPOINT_ROOM = "/room"
+ENDPOINT_BED = "/bed"
 
 def settings_exits() -> bool:
     return os.path.isfile(SETTINGS_FILE_NAME)
@@ -13,13 +14,14 @@ def settings_exits() -> bool:
 def load_settings() -> Tuple[str, int]:
     with open(SETTINGS_FILE_NAME) as settings_file:
         settings = json.load(settings_file)
-        return (settings["server_host"], settings["room_id"], settings["room_name"])
-
-def save_settings(server_host : str, room_id : int, room_name : str) -> None:
+        return (settings["server_host"], settings["room_id"], settings["room_name"], settings["bed_id"])
+    
+def save_settings(server_host : str, room_id : int, room_name : str, bed_id : int) -> None:
     settings = {}
     settings["server_host"] = server_host
     settings["room_id"] = room_id
     settings["room_name"] = room_name
+    settings["bed_id"] = bed_id
 
     with open(SETTINGS_FILE_NAME, "w") as settings_file:
         json.dump(settings, settings_file)
@@ -30,7 +32,7 @@ def change_settings():
     response = requests.get(server_host)
 
     if response.status_code == 200:
-        rooms = requests.get(server_host + ENPOINT_ROOM + "/list")
+        rooms = requests.get(server_host + ENDPOINT_ROOM + "/list")
         if rooms is not None:
             try:
                 rooms_json = rooms.json()
@@ -46,14 +48,38 @@ def change_settings():
                 chosen_room = int(input("Choose a room by its number: "))
                 try:
                     room = rooms_json[chosen_room - 1]
-                    save_settings(server_host, room["id"], room["name"])
+                    bed = choose_bed(server_host, room["id"])
+                    save_settings(server_host, room["id"], room["name"], bed["id"])
                     print("\n*SETUP COMPLETED*")
                 except IndexError:
                     print("ERROR: The room doesn't exits")
             else:
-                print("*NO ROOMS AVAILBALE*")
+                print("*NO ROOMS AVAILABLE*")
     else:
         print("ERROR: The server can't be reached")
+
+def choose_bed(server_host : str, room_id : int):
+    url = ("""%s%d""" % (server_host + ENDPOINT_BED + "/all?room_id=", room_id))
+    beds = requests.get(url)
+    try:
+        beds_json = beds.json()
+    except ValueError:
+        print("ERROR: Bad rersponse")
+        exit()
+    
+    if len(beds_json) > 0:
+        count = 1
+        for bed in beds_json:
+            print("%d) %s" % (count, bed["id"]))
+            count += 1
+        chosen_bed = int(input("Choose a bed by its number: "))
+        try:
+            bed = beds_json[chosen_bed - 1]
+            return bed
+        except IndexError:
+            print("ERROR: The bed doesn't exits")
+    else:
+        print("*NO BEDS AVAILABLE*")
 
 #Running code
 if settings_exits():
@@ -66,4 +92,5 @@ if settings_exits():
     if change == 'y':
         change_settings()
 else:
+    print("qui")
     change_settings()
--- server/models/bed.py
+++ server/models/bed.py
@@ -75,6 +75,34 @@ class Bed:
             if(mydb.is_connected()):
                 mydb.close()
 
+    def get_bed_list(self):
+        mydb = None
+        try:
+            mydb = mysql.connector.connect(
+                user = os.getenv("DATABASE_USER"),
+                database = os.getenv("DATABASE_NAME"),
+                password = os.getenv("DATABASE_PASSWORD")
+            )
+            cursor = mydb.cursor()
+            val = (self.room_id)
+            sql = ("""SELECT * FROM bed WHERE id = %d""" % val)
+
+            cursor.execute(sql)
+            columns = [column[0] for column in cursor.description]
+            beds = []
+            for row in cursor.fetchall():
+                beds.append(dict(zip(columns, row)))
+            
+            return beds
+        except Exception as e:
+            print(e)
+            return 500
+        finally:
+            if(mydb.is_connected()):
+                mydb.close()
+
+
+
 bed_blueprint = Blueprint('bed', __name__)
 
 @bed_blueprint.route("/add", methods=["POST"]) #Add a new bed
@@ -96,6 +124,19 @@ def get():
     if(bed_id is not None):
         obj = Bed(None, None, None)
         value = obj.get_bed(bed_id)
+        if(value != 500):
+            return jsonify(value)
+        else:
+            return abort(value)
+    else:
+        return abort(400)
+
+@bed_blueprint.route("/all", methods=["GET"]) #Get the list of all beds in a room
+def get_all():
+    room_id = request.args.get("room_id", default=None, type=int)
+    if(room_id is not None):
+        obj = Bed(None, None, room_id)
+        value = obj.get_bed_list()
         if(value != 500):
             return jsonify(value)
         else:
